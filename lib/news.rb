d74bf81ecfa2599e161b0ed59428f24ace8e96b8
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-06-16 09:03:58 +0000

    Add the class News to the news.rb file

---
+++ lib/news.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rss'
+require 'open-uri'
+
+class News
+  attr_writer :url
+  def initialize
+    @url = 'http://feeds.bbci.co.uk/news/world/rss.xml#'
+    @news_item = nil
+  end
+
+  def send_news
+    URI.parse(@url).open do |rss|
+      feed = RSS::Parser.parse(rss)
+      item_index = rand(feed.items.size - 1)
+      @news_item =
+        "#{feed.items[item_index].title}\n#{feed.items[item_index].link}"
+    end
+  end
+end
