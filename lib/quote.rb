855a3e053d3d96192faade21b28212274826437f
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-06-16 09:06:04 +0000

    Add the class Quotes to quote.rb file

---
+++ lib/quote.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'telegram_bot'
+require 'net/http'
+require 'json'
+require_relative 'bot.rb'
+
+class Quotes
+  @values = nil
+
+  def initialize
+    @values = make_request
+  end
+
+  def make_request
+    url = 'https://type.fit/api/quotes'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response = JSON.parse(response)
+    response
+  end
+
+  def select_random
+    @values = @values.sample
+    @values
+  end
+end
