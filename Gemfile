cc8e807c01f3fd809fa094282088cd4c734d2a5d
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-06-17 20:30:32 +0000

    update files

--- Gemfile
+++ Gemfile
@@ -3,12 +3,12 @@ source 'https://rubygems.org'
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-# source 'https://rubygems.org'
+source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
+gem 'dotenv'
 gem 'rspec'
 gem 'telegram_bot'
-gem 'dotenv'
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,5 @@
 # rubocop :disable Metrics/MethodLength, Layout/LineLength
+
 require 'telegram_bot'
 require_relative './news.rb'
 require_relative '../lib/quotes.rb'
@@ -18,20 +19,20 @@ class Bot
       command = message.get_command_for(@bot)
       message.reply do |reply|
         reply.text = case command.downcase
-          when /start/i
-            "Hey, I\'m still unnder experimentation. But I can do quite a few interesting things. Try typing any of these commands:\n/hello\n/quote\n/latest news"
-          when /hello/i
-            "Hello, #{message.from.first_name}"
-          when /quote/i
-            quates = Quotes.new
-            quote = quates.select_random
-            "#{quote["text"]}\n\t\t\t #{quote["author"]}"
-          when /latest news/i
-            news = News.new
-            news.send_news
-          else
-            "Oops... I'm still learning. I have no idea what #{command.inspect} means. I suggest you type commands like\n/hello\n/quote\n/latest news for now."
-          end
+                     when /start/i
+                       "Hey, I\'m still unnder experimentation. But I can do quite a few interesting things. Try typing any of these commands:\n/hello\n/quote\n/latest news"
+                     when /hello/i
+                       "Hello, #{message.from.first_name}"
+                     when /quote/i
+                       quates = Quotes.new
+                       quote = quates.select_random
+                       "#{quote['text']}\n\t\t\t #{quote['author']}"
+                     when /latest news/i
+                       news = News.new
+                       news.send_news
+                     else
+                       "Oops... I'm still learning. I have no idea what #{command.inspect} means. I suggest you type commands like\n/hello\n/quote\n/latest news for now."
+                     end
         reply.send_with(@bot)
       end
     end
